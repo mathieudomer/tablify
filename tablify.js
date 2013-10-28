b7538c38da0436143a376d1d924f314ce29fc7e9
mathieudomer <Mathieu@macbook.home>
2013-10-28 22:48:05 +0100

    Hover et styles

--- tablify.js
+++ tablify.js
@@ -3,7 +3,18 @@
  * Author : Mathieu DOMER
  * Created : 25/10/2013
  * Modified : 
- *
+ * Backlog : 
+ * - Sauvegarder en local les colonnes
+ * - clic droit
+ * - Drag'n drop colonne
+ * - Tri
+ * - Filtre
+ * - Editer
+ * - Historique
+ * - Enregistrer
+ * - Charger
+ * - Générer le HTML
+ * - Websockets ? (modification côté serveur)
  */
 
 
@@ -29,16 +40,35 @@ NodeList.prototype.addEventListenerAll = function(type, listener, useCapture) {
  * @return	True - The elt is in an element which class name is className
  * @return	False - The elt is not in an element which class name is className
  */
-// Fonctions assistance
 var isIn = function(elt, className) {
 	if(elt == document.body || elt == document.body.parentNode) return false;
 	else if(elt.parentNode.classList.contains(className)) return true;
 	else return isIn(elt.parentNode, className);
 };
 
-// Génération du HTML
+/**
+ * Recalculate row ids
+ *
+ */
+var recalculateRowIds = function() {
+	var trs = document.querySelectorAll(".tablify tbody tr");
+	for (var i = 0; i < trs.length; i++) {
+		trs[i].id = "tr" + i;
+		trs[i].firstElementChild.innerHTML = i + 1;
+	}
+};
 
-// Events 
+/**
+ * Extends {@link Element} to determine a child node's index inside of its parent node 
+ *
+ * @return	The index
+ */
+Element.prototype.getIndex = function() {
+	var i = 0;
+	var child = this;
+	while((child = child.previousElementSibling) != null) i++;
+	return i;
+}
 
 /**
  * Defines behavior of element of tbody is selected
@@ -48,14 +78,20 @@ var isIn = function(elt, className) {
  * @see 	EventTarget.addEventListener
  */
 Tablify.prototype.tbodyClick = function(e) {
-	var trsSelected = document.querySelectorAll(".tablify tr.selected");
+	// Unselect all selected rows if neither ctrl nor  cmd key are pressed
 	if(!e.ctrlKey && !e.metaKey) {
-		// Déselection des lignes sélectionnées si ctrl n'est pas appuyée
+		var trsSelected = document.querySelectorAll(".tablify tr.selected");
 		for(var i = 0; i < trsSelected.length; i++) {
 			trsSelected[i].classList.remove("selected");
 		}
 	}
+	// Row selection
 	e.target.parentNode.classList.add("selected");
+	// Add buttons inactivation
+	var addButtons = document.querySelectorAll(".tablify button.add");
+	for(var i = 0; i < addButtons.length; i++) {
+		addButtons[i].disabled = "disabled";
+	}
 };
 
 /**
@@ -66,12 +102,42 @@ Tablify.prototype.tbodyClick = function(e) {
  * @see 	EventTarget.addEventListener
  */
 Tablify.prototype.documentClick = function(e) {
+	// Unselect selected rows if the click is outside the table
 	if (!isIn(e.target, "tablify")) {
 		var trsSelected = document.querySelectorAll(".tablify tr.selected");
 		for(var i = 0; i < trsSelected.length; i++) {
 			trsSelected[i].classList.remove("selected");
 		}
+		// Add buttons reactivation
+		var addButtons = document.querySelectorAll(".tablify button.add");
+		for(var i = 0; i < addButtons.length; i++) {
+			addButtons[i].removeAttribute("disabled");
+		}
 	}
+};
+
+/**
+ * Defines behavior tr elements on mouse enter
+ * Add .hovered class to first td of the row and to the corresponding header th of the column
+ *
+ * @param	e 	{@link Event} object
+ * @see 	EventTarget.addEventListener
+ */
+Tablify.prototype.tdsMouseEnter = function(e) { 
+	e.target.parentNode.firstElementChild.classList.add("hovered");
+	document.querySelector(".tablify thead th:nth-child(" + (e.target.getIndex() + 1) + ")").classList.add("hovered");
+};
+
+/**
+ * Defines behavior tr elements on mouse leave
+ * Remove .hovered class to first td of the row and to the corresponding header th of the column
+ *
+ * @param	e 	{@link Event} object
+ * @see 	EventTarget.addEventListener
+ */
+Tablify.prototype.tdsMouseLeave = function(e) { 
+	e.target.parentNode.firstElementChild.classList.remove("hovered");
+	document.querySelector(".tablify thead th:nth-child(" + (e.target.getIndex() + 1) + ")").classList.remove("hovered");
 };
 
 /**
@@ -110,6 +176,7 @@ Tablify.prototype.trsDragOver = function(e) {
 			tbody.insertBefore(document.querySelector(".tablify tr.moving"), trOvered.nextElementSibling);
 		}
 	}
+	recalculateRowIds();
 	return false;
 };
 
@@ -124,11 +191,7 @@ Tablify.prototype.trsDragOver = function(e) {
 Tablify.prototype.trsDragEnd = function(e){
 	e.preventDefault();
 	document.querySelector(".tablify tr.moving").classList.remove("moving");
-	// Refaire les ids
-	var trs = document.querySelectorAll(".tablify tbody tr");
-	for (var i = 0; i < trs.length; i++) {
-		trs[i].id = "tr" + i;
-	}
+	recalculateRowIds();
 	return false;
 };
 
@@ -143,19 +206,20 @@ Tablify.prototype.tbodyDrop = function(e){
 	e.preventDefault();
 	return false;
 };
-// Tri
 
+/**
+ * Defines behavior of the tbody elements on drop
+ * Open an inline form for row editing values
+ *
+ * @param	e 	{@link Event} object
+ * @see 	EventTarget.addEventListener
+ */
+Tablify.prototype.tbodyDblClick = function(e){
+	e.target.parentNode.classList.remove("selected");
+	e.target.parentNode.classList.toggle("editing");
 
-// Filtre
-
-
-// Editer
-
-
-// Ajouter
-
-
-// Enregistrer
+	// Disable add and remove buttons
+};
 
 /**
  * Defines behavior of the Remove button when clicked
@@ -164,10 +228,16 @@ Tablify.prototype.tbodyDrop = function(e){
  * @param	e 	{@link Event} object
  * @see 	EventTarget.addEventListener
  */
-Tablify.prototype.supprimersClick = function(e) {
+Tablify.prototype.removeButtonsClick = function(e) {
 	var trsSelected = document.querySelectorAll(".tablify tr.selected");
 	for(var i = 0; i < trsSelected.length; i++) {
 		document.querySelector(".tablify tbody").removeChild(trsSelected[i]);
+	}
+	recalculateRowIds();
+	// Add buttons reactivation
+	var addButtons = document.querySelectorAll(".tablify button.add");
+	for(var i = 0; i < addButtons.length; i++) {
+		addButtons[i].removeAttribute("disabled");
 	}
 };
 
@@ -179,16 +249,21 @@ function Tablify() {
 	// Propriétés
 	this.tbody = document.querySelector(".tablify tbody");
 	this.trs = document.querySelectorAll(".tablify tbody tr");
-	this.removeButton = document.querySelectorAll(".tablify button.remove");
+	this.tdsRowIndex = document.querySelectorAll(".tablify tbody td:first-child");
+	this.tdsField = document.querySelectorAll(".tablify tbody td:not(:first-child)");
+	this.removeButtons = document.querySelectorAll(".tablify button.remove");
 	
 	// Initiate events
 	document.addEventListener("click", this.documentClick, false);
 	this.tbody.addEventListener("click", this.tbodyClick, false);
+	this.tbody.addEventListener("dblclick", this.tbodyDblClick, false);
+	this.tbody.addEventListener("drop", this.tbodyDrop, false);
 	this.trs.addEventListenerAll("dragstart", this.trsDragStart, false);
 	this.trs.addEventListenerAll("dragover", this.trsDragOver, false);
 	this.trs.addEventListenerAll("dragend", this.trsDragEnd, false);
-	this.tbody.addEventListener("drop", this.tbodyDrop, false);
-	this.removeButton.addEventListenerAll("click", this.supprimersClick, false);
+	this.tdsField.addEventListenerAll("mouseenter", this.tdsMouseEnter, false);
+	this.tdsField.addEventListenerAll("mouseleave", this.tdsMouseLeave, false);
+	this.removeButtons.addEventListenerAll("click", this.removeButtonsClick, false);
 
 }
 var test = new Tablify();
